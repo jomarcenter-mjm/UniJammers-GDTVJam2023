5c25246532730d4a00a70840f979d4590066c7d7
Ste-Gar <ste.gargiulo@gmail.com>
2023-05-20 17:07:31 +0200

    Input Reader and basic topdown controller

---
+++ Assets/Scripts/Controls/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using uj.input;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    InputReader inputReader;
+    CharacterController charController;
+
+    Vector2 moveInput;
+
+    [SerializeField] float moveSpeed = 5;
+
+    private void Start()
+    {
+        inputReader = FindObjectOfType<InputReader>();
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        moveInput = inputReader.GetMoveInput();
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
+
+        charController.Move(moveSpeed * Time.deltaTime * movement);
+    }
+}
---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,31 @@
+using uj.input.actions;
+using UnityEngine;
+
+namespace uj.input
+{
+    public class InputReader : MonoBehaviour
+    {
+        InputActions inputActions;
+
+        private void Awake()
+        {
+            inputActions = new InputActions();
+            inputActions.Enable();
+        }
+
+        private void OnDestroy()
+        {
+            inputActions.Disable();
+        }
+
+        public Vector2 GetMoveInput()
+        {
+            return inputActions.Player.Move.ReadValue<Vector2>();
+        }
+
+        public Vector2 GetLookInput()
+        {
+            return inputActions.Player.Move.ReadValue<Vector2>();
+        }
+    }
+}
