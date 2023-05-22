4b14d91eb4bbecc47a97ce0293c2d75f71ff8e4d
Ste-Gar <ste.gargiulo@gmail.com>
2023-05-22 13:19:44 +0200

    Changed camera switch to the input system. Commented logs.

--- Assets/Scripts/Controls/DimensionTransition.cs
+++ Assets/Scripts/Controls/DimensionTransition.cs
@@ -1,16 +1,17 @@
+using uj.input;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RenderTextureSwitcher : MonoBehaviour
 {
     [SerializeField] private Camera fPCam;   //first parent camera selectebale
-   [SerializeField] private Camera tDCam;   //Top Down camera selectable
-   [SerializeField] private RawImage smallScreenRawImage;  // RawImage component to display the camera output
+    [SerializeField] private Camera tDCam;   //Top Down camera selectable
+    [SerializeField] private RawImage smallScreenRawImage;  // RawImage component to display the camera output
     [SerializeField] private RawImage largeSCreenRawImage;  // RawImage component to display the camera output
 
-    
+    InputReader inputReader;
 
-   private void Start()
+    private void Start()
     {
         // Make sure at least one camera and RawImage are assigned
         if (fPCam == null || tDCam == null)
@@ -25,39 +26,39 @@ public class RenderTextureSwitcher : MonoBehaviour
             Debug.LogError("An image is not assigned");
             return;
 
-        }        
+        }
+
+        inputReader = FindObjectOfType<InputReader>();
     }
 
     private void Update()
     {
         // Check for input to switch camera outputs
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (inputReader.GetCameraSwitchPressedThisFrame())
         {
-            
             SwitchScreens();
-                       
         }
     }
-    private void SwitchScreens()
-        {
-        Debug.Log("small = " + smallScreenRawImage.texture + "Large = " +largeSCreenRawImage.texture +  "\nfpCam = " + fPCam.targetTexture +"tDCam = " + tDCam.targetTexture  );
 
+    private void SwitchScreens()
+    {
         //Reassign the target Textures to the other camera.
+
+        //Debug.Log("small = " + smallScreenRawImage.texture + "Large = " + largeSCreenRawImage.texture + "\nfpCam = " + fPCam.targetTexture + "tDCam = " + tDCam.targetTexture);
         fPCam.enabled = false;
         tDCam.enabled = false;
         if (smallScreenRawImage.texture == fPCam.targetTexture)
-        {            
+        {
             smallScreenRawImage.texture = tDCam.targetTexture;
             largeSCreenRawImage.texture = fPCam.targetTexture;
-        } else 
-        {            
+        }
+        else
+        {
             smallScreenRawImage.texture = fPCam.targetTexture;
             largeSCreenRawImage.texture = tDCam.targetTexture;
         }
         fPCam.enabled = true;
         tDCam.enabled = true;
-        Debug.Log("AFTER small = " + smallScreenRawImage.texture + "Large = " + largeSCreenRawImage.texture + "\nfpCam = " + fPCam.targetTexture + "tDCam = " + tDCam.targetTexture);
-       
-
+        //Debug.Log("AFTER small = " + smallScreenRawImage.texture + "Large = " + largeSCreenRawImage.texture + "\nfpCam = " + fPCam.targetTexture + "tDCam = " + tDCam.targetTexture);
     }
 }
